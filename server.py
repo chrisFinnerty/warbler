b88a359580785df5b664cec1203895f91033b47c
chris <chrisjfinnerty@gmail.com>
2024-07-09 14:14:50 -0700

    Render Deploy#3

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 from app import create_app
 from models import connect_db
 
-load_dotenv()
+# load_dotenv()
 
-app = create_app(os.environ.get('DATABASE_URL'))
+app = create_app('warbler')
 connect_db(app)
